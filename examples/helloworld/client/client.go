0ce1d131f859d911efe44c7d89e3c9d687569095
qianxi0410 <894871277@qq.com>
2021-12-03 10:43:11 +0800

    chore: update exp helloworld

--- examples/helloworld/client/client.go
+++ examples/helloworld/client/client.go
@@ -3,32 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/qianxi0410/naive-rpc/client"
 	"github.com/qianxi0410/naive-rpc/client/selector"
 	"github.com/qianxi0410/naive-rpc/codec/evangelion"
+	"github.com/qianxi0410/naive-rpc/config"
 	"google.golang.org/protobuf/proto"
 )
 
 func main() {
+	y, err := config.NewYamlConfig("../conf/helloworld.yaml")
+	if err != nil {
+		log.Fatalf("read config file err: %s", err.Error())
+	}
+
+	server_addr := y.Read("server_addr", "")
+	// user_id := y.Read("user_id", "")
+	// user_key := y.Read("user_key", "")
+
 	cli := client.NewClient("test",
-		client.WithAddr("127.0.0.1:8888"),
+		client.WithAddr(server_addr),
 		client.WithCodec(evangelion.NAME),
 		client.WithTransportType(client.TCP4),
-		client.WithSelector(selector.NewIPSelector("tcp4", []string{"127.0.0.1:8888"})),
+		client.WithSelector(selector.NewIPSelector("tcp4", []string{server_addr})),
 	)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
 	defer cancel()
 
 	reqHead := &evangelion.Request{
-		Seqno:   proto.Uint64(1000),
-		Appid:   proto.String("strong"),
-		Rpcname: proto.String("/hello"),
-		Userid:  proto.String("qianxi"),
-		Userkey: proto.String("cat"),
-		Version: proto.Uint32(0),
+		// Seqno:   proto.Uint64(1000),
+		// Appid:   proto.String("strong"),
+		Rpcname: proto.String("/ping"),
+		// Userid:  proto.String(user_id),
+		// Userkey: proto.String(user_key),
+		// Version: proto.Uint32(0),
 	}
 	fmt.Println(reqHead)
 
--- examples/helloworld/server/server.go
+++ examples/helloworld/server/server.go
@@ -3,33 +3,54 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/qianxi0410/naive-rpc/codec/evangelion"
+	"github.com/qianxi0410/naive-rpc/config"
 	"github.com/qianxi0410/naive-rpc/router"
 	"github.com/qianxi0410/naive-rpc/transport"
-	"google.golang.org/protobuf/proto"
 )
 
 func main() {
+	y, err := config.NewYamlConfig("../conf/helloworld.yaml")
+	if err != nil {
+		log.Fatalf("load config error %s", err.Error())
+	}
+
+	addr := y.Read("server_addr", "")
+
 	r := router.NewRouter()
 	r.Forward("/hello", func(ctx context.Context, req interface{}) (rsp interface{}, err error) {
 		pbreq := req.(*evangelion.Request)
 		fmt.Printf("server recv req:%v", pbreq)
 
 		pbrsp := &evangelion.Response{
-			Seqno:   pbreq.Seqno,
-			ErrCode: proto.Uint32(0),
-			ErrMsg:  proto.String("success"),
-			Body:    []byte("hello " + *pbreq.Userid),
+			// Seqno:   pbreq.Seqno,
+			// ErrCode: proto.Uint32(0),
+			// ErrMsg:  proto.String("success"),
+			Body: []byte("hello world"),
+		}
+		return pbrsp, nil
+	})
+
+	r.Forward("/ping", func(ctx context.Context, req interface{}) (rsp interface{}, err error) {
+		pbreq := req.(*evangelion.Request)
+		fmt.Printf("server recv req:%v", pbreq)
+
+		pbrsp := &evangelion.Response{
+			// Seqno:   pbreq.Seqno,
+			// ErrCode: proto.Uint32(0),
+			// ErrMsg:  proto.String("success"),
+			Body: []byte("pong"),
 		}
 		return pbrsp, nil
 	})
 
 	tcpSvr, err := transport.NewTcpServerTransport(context.TODO(),
-		"tcp4", "127.0.0.1:8888", evangelion.NAME, transport.WithRouter(r))
+		"tcp4", addr, evangelion.NAME, transport.WithRouter(r))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("server is on %s", tcpSvr.Address())
+	fmt.Printf("server is on %s\n", tcpSvr.Address())
 	tcpSvr.ListenAndServe()
 }
